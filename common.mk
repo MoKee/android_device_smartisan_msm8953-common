8a968caf43826193fc29846a488fbac786d394cd
XiNGRZ <chenxingyu92@gmail.com>
2019-01-03 22:16:20 +0800

    HAX: Kill failing modules
    Change-Id: I1e0d1726364f6b76fbadb84258fc2391ac83ee34

--- common.mk
+++ common.mk
@@ -207,8 +207,8 @@ PRODUCT_BOOT_JARS += \
     ifaa_fw
 
 # Input
-PRODUCT_PACKAGES += \
-    KeyHandlerSmartisan
+# PRODUCT_PACKAGES += \
+#     KeyHandlerSmartisan
 
 # IPv6
 PRODUCT_PACKAGES += \
