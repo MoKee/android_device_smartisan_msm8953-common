fc65cb985e5f2821a79a714b9936ff5d4ec1be0c
XiNGRZ <chenxingyu92@gmail.com>
2019-01-24 14:38:24 +0800

    msm8953: Build device-specific camera provider service
    Change-Id: Ifde18871197e45e9ea8fcb0f97c05f24b8faa59f

--- common.mk
+++ common.mk
@@ -116,8 +116,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
-    android.hardware.camera.provider@2.4-impl:32 \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-impl:32
 
 PRODUCT_PACKAGES += \
     Snap
